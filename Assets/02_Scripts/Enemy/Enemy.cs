b1f2cb232f30ce8d5b44421bfa6e66d69d38d6df
함동현 <>
2026-06-15 18:30:52 +0900

    feat: Enemy 에셋, 애니메이션, 스크립트 기초 작성

---
+++ Assets/02_Scripts/Enemy/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Animator _anim;
+
+    private void Start()
+    {
+        // 애니메이션을 찾고 그 애니메이션을 담을 변수인 _anim으로 지정
+        _anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+
+    }
+}
