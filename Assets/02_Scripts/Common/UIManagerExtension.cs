2b0a825095c292bb5bf0eb70667bd26210caa109
Kimikhwan <moonfgg@naver.com>
2026-06-16 21:10:17 +0900

    fix: 새롭게 추가된 스크립트 기존 스크립트에 적용

--- Assets/02_Scripts/Common/UIManagerExtension.cs
+++ Assets/02_Scripts/Common/UIManagerExtension.cs
@@ -1,4 +1,4 @@
-/// <summary>
+﻿/// <summary>
 /// UI 프리팹이 배치될 Canvas 루트 종류입니다.
 /// </summary>
 public enum UIRootType
@@ -18,7 +18,8 @@ public enum UIType
 {
     None = 0,
     MainUI,
-    LoadingUI
+    LoadingUI,
+    CenterPointUI
 }
 
 /// <summary>
@@ -41,6 +42,12 @@ public static class UIManagerExtension
     {
         uiManager.OpenLoadingUI();
         uiManager.OpenUI(UIRootType.MainUI, UIType.MainUI);
+        uiManager.OpenCenterPointUI();
+
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.LockGameplayCursor();
+        }
     }
 
     /// <summary>
@@ -57,5 +64,47 @@ public static class UIManagerExtension
     public static void CloseLoadingUI(this UIManager uiManager)
     {
         uiManager.CloseUI(UIType.LoadingUI);
+    }
+
+    /// <summary>
+    /// 게임 플레이 중 화면 중앙 기준점을 보여주는 UI를 엽니다.
+    /// </summary>
+    public static UIBase OpenCenterPointUI(this UIManager uiManager)
+    {
+        return uiManager.OpenUI(UIRootType.VeryFrontUI, UIType.CenterPointUI);
+    }
+
+    /// <summary>
+    /// 게임 플레이 중 화면 중앙 기준점을 보여주는 UI를 닫습니다.
+    /// </summary>
+    public static void CloseCenterPointUI(this UIManager uiManager)
+    {
+        uiManager.CloseUI(UIType.CenterPointUI);
+    }
+
+    /// <summary>
+    /// 게임 플레이 화면용 중앙 포인터 UI를 켜고 마우스 커서를 잠급니다.
+    /// </summary>
+    public static void EnterGameplayCursorMode(this UIManager uiManager)
+    {
+        uiManager.OpenCenterPointUI();
+
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.LockGameplayCursor();
+        }
+    }
+
+    /// <summary>
+    /// UI 조작 화면용으로 중앙 포인터 UI를 끄고 마우스 커서 잠금을 해제합니다.
+    /// </summary>
+    public static void ExitGameplayCursorMode(this UIManager uiManager)
+    {
+        uiManager.CloseCenterPointUI();
+
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.UnlockGameplayCursor();
+        }
     }
 }
