02605398235fcbb67b66439cc132cb9ade4bda47
이광호 <rhkdghgha@gmail.com>
2026-06-17 17:46:42 +0900

    feat : 아이템 종류 세분화, 아이템 데이터 추가

--- Assets/02_Scripts/Item/ItemBase.cs
+++ Assets/02_Scripts/Item/ItemBase.cs
@@ -2,7 +2,7 @@
 public enum ItemType 
 {
     None =0,
-    Consumeable,
+    Potion,
     Tool,
     Jewel
 }
@@ -15,10 +15,25 @@ public enum ItemGrade
     Legendary
 }
 public class ItemBase : MonoBehaviour
-{
-    public string ItemId { get; private set; }
+{ 
+    public string Id { get; private set; }
+    public ItemType CurrentItemType { get; private set; }
+    public ItemGrade CurrentItemGrade { get; private set; }
+   
     public float Weight { get; private set; }
     public int Price { get; private set; }
-    public ItemType Type { get; private set; }
-    public ItemGrade Grade { get; private set; }
+    
+    public virtual void  InitItem(ItemData data) 
+    {
+        if (data == null)
+        {
+            Debug.LogError("데이터 없음");
+            return;
+        }
+        Id = data.Id;
+        CurrentItemType = data.CurrentItemType;
+        CurrentItemGrade = data.CurrentItemGrade;
+        Weight = data.Weight;
+        Price = data.Price;    
+    }
 }
