f0c3f212996750b55285dd26e3c5e15a4a014a56
Kimikhwan <moonfgg@naver.com>
2026-06-22 21:06:14 +0900

    feat: 스폰 프로토타입 - 특정 영역을 설정하여 랜덤으로 소환
    우선 바닥과 벽에 관란 로직만 있으며, 프로토타입 입니다.

---
+++ Assets/02_Scripts/Map/Spawn/FloorSpawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class FloorSpawner : MonoBehaviour
+{
+    [SerializeField] private int _spawnCount = 3;
+    [SerializeField] private int _spawnTryCount = 30;
+    [SerializeField] private SpawnArea[] _spawnArea;
+    [SerializeField] private GameObject _tempPrefab;
+    [SerializeField] private LayerMask _floorLayer;
+    [SerializeField] private LayerMask _obstacleLayer;
+    [SerializeField] private float _rayDistance = 20f;
+    [SerializeField] private Vector3 _checkHalfExtents = new Vector3(0.5f, 0.5f, 0.5f);
+
+    private int _spawnedCount = 0;
+
+    public void SpawnObjectFromFloor()
+    {
+        while (_spawnedCount < _spawnCount)
+        {
+            for (int i = 0; i < _spawnTryCount; i++)
+            {
+                SpawnArea volume = _spawnArea[Random.Range(0, _spawnArea.Length)];
+                Vector3 randomPoint = volume.GetRandomPosition();
+
+                if (!Physics.Raycast(randomPoint, Vector3.down, out RaycastHit hit, _rayDistance, _floorLayer))
+                    continue;
+
+                Vector3 spawnPos = hit.point;
+
+                if (Physics.CheckBox(spawnPos, _checkHalfExtents, Quaternion.identity, _obstacleLayer))
+                    continue;
+
+                Instantiate(_tempPrefab, spawnPos, Quaternion.identity);
+                _spawnedCount++;
+                return;
+            }
+        }
+    }
+}
+
---
+++ Assets/02_Scripts/Map/Spawn/WallSpawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class WallSpawner : MonoBehaviour
+{
+    [SerializeField] private int _spawnCount = 3;
+    [SerializeField] private int _spawnTryCount = 30;
+    [SerializeField] private SpawnArea[] _spawnArea;
+    [SerializeField] private GameObject _tempPrefab;
+    [SerializeField] private LayerMask _wallLayer;
+    [SerializeField] private LayerMask _obstacleLayer;
+    [SerializeField] private float _rayDistance = 20f;
+    [SerializeField] private Vector3 _checkHalfExtents = new Vector3(0.5f, 0.5f, 0.5f);
+
+    private int _spawnedCount = 0;
+
+    public void SpawnObjectFromWall()
+    {
+        Vector3[] directions = { Vector3.forward, Vector3.back, Vector3.left, Vector3.right };
+
+        for (int i = 0; i < _spawnTryCount; i++)
+        {
+            SpawnArea volume = _spawnArea[Random.Range(0, _spawnArea.Length)];
+            Vector3 randomPoint = volume.GetRandomPosition();
+
+            Vector3 dir = directions[Random.Range(0, directions.Length)];
+
+            if (!Physics.Raycast(randomPoint, dir, out RaycastHit hit, _rayDistance, _wallLayer))
+                continue;
+
+            Vector3 spawnPos = hit.point;
+            Quaternion spawnRot = Quaternion.LookRotation(-hit.normal);
+
+            if (Physics.CheckBox(spawnPos, _checkHalfExtents, spawnRot, _obstacleLayer))
+                continue;
+
+            Instantiate(_tempPrefab, spawnPos, spawnRot);
+            //_spawnedCount++;
+            return;
+        }
+    }
+}
