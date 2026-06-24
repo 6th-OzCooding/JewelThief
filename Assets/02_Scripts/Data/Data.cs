b55cfcbcfd2ad26ab552ff141240e6c428d120cc
iThing1 <r930324@gmail.com>
2026-06-24 13:44:01 +0900

    Revert "Hotfix/Conflict: merge에러 해결 중 삭제 데이터 복구"

--- Assets/02_Scripts/Data/Data.cs
+++ Assets/02_Scripts/Data/Data.cs
@@ -8,25 +8,6 @@ public enum SoundType
     Voice
 }
 
-public enum PopupType
-{
-    None = 0,
-    Simple,
-    ItemInfo,
-    ShopInfo
-}
-
-public enum PopupTargetType
-{
-    None = 0,
-    Item,
-    Box,
-    Trap,
-    EscapePath,
-    Door,
-    Tool
-}
-
 [Serializable]
 public class BaseData
 {
@@ -135,20 +116,4 @@ public class StageData : BaseData
     public int MaxTrap;
     public int ExitCount;
     public int EnemyId;
-}
-
-[Serializable]
-public class PopupViewData : BaseData
-{
-    public string PopupType;
-    public string DefaultPrompt;
-    public string LockedPrompt;
-    public string MasterKeyPrompt;
-    public string MasterKeyLimitPrompt;
-    public string OverweightPrompt;
-    public string NotEnoughMoneyPrompt;
-    public string PurchaseSuccessPrompt;
-
-    public global::PopupType GetPopupType()
-        => Enum.TryParse<global::PopupType>(PopupType, out var result) ? result : global::PopupType.Simple;
 }
