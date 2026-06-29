288ae8167e54c782a967ce7ff8b45805562e8c26
Kimikhwan <moonfgg@naver.com>
2026-06-30 01:36:04 +0900

    feat: Turret 함정 추가
    - 플레이어 감지 -> 예열 시간 -> 18발 발사 -> 장전 -> 플레이어 감지

---
+++ Assets/02_Scripts/Projectile/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _lifeTime = 5f;
+
+    Vector3 _moveDirection;
+
+    public void Init(Vector3 direction)
+    {
+        _moveDirection = direction.normalized;
+        transform.rotation = Quaternion.LookRotation(_moveDirection);
+        Destroy(gameObject, _lifeTime);
+    }
+
+    private void Update()
+    {
+        transform.position += _moveDirection * _speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log($"Bullet hit: {other.gameObject.name}");
+            Destroy(gameObject);
+        }
+    }
+}
